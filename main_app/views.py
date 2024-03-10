a1a2947328e4899f403555363479a4f01aacc9b4
Artem Davtyan <artem_davtyan7@mail.ru>
2024-03-10 14:16:29 +0300

    main app set up * change index view * change base.html * change index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .forms import TaskListForm
+from .models import TaskList
 
 
 def index(request):
-    return render(request, 'main_app/index.html')
+    if not request.session.session_key:
+        request.session.save()
+    session_key = request.session.session_key
+    user_tasklist = TaskList.objects.filter(session_key=session_key)
+    if request.method == 'POST':
+        form = TaskListForm(request.POST, initial={'session_key': session_key})
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+    else:
+        form = TaskListForm()
+    return render(request, 'main_app/index.html', {'form': form, 'user_tasklist': user_tasklist})
