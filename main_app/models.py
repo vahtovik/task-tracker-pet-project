165e23c86af7a1e50a8189e0da6323f7e035665a
Artem Davtyan <artem_davtyan7@mail.ru>
2024-03-10 14:08:15 +0300

    add TaskList model * migrations applied

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskList(models.Model):
+    session_key = models.CharField(max_length=40, null=True, blank=True)
+    task = models.CharField(max_length=255)
+    # task_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.task
