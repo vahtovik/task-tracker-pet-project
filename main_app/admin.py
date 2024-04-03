e0c372d642409523201a3885f1e51373981d6142
Artem Davtyan <artem_davtyan7@mail.ru>
2024-04-03 23:11:13 +0300

    create TaskListAdmin class * set up fields displayment in admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import TaskList
 
-admin.site.register(TaskList)
+
+@admin.register(TaskList)
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task_name', 'is_active', 'is_completed',
+                    'completed_task_start_time', 'completed_task_end_time')
+    list_display_links = ('user', 'task_name')
+    fields = ('user', 'task_name', 'is_active', 'is_completed', 'completed_task_start_time', 'completed_task_end_time')
