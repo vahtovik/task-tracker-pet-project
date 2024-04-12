a1c15db473edd74cd23577e14cb6d9b496f24c45
Artem Davtyan <artem_davtyan7@mail.ru>
2024-04-12 22:21:11 +0300

    add EditCredentialsTestCase * cover 9 test cases

--- users/tests.py
+++ users/tests.py
@@ -95,3 +95,83 @@ class AuthenticationTestCase(TestCase):
         path = reverse('main_app:index')
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class EditCredentialsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        self.client.login(username='test_user', password='test_password')
+
+    def test_access_via_get(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_edit_of_all_credentials(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {
+            'username': 'new_test_user',
+            'new_password1': 'new_test_password',
+            'new_password2': 'new_test_password',
+        })
+        self.user.refresh_from_db()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTrue(response.json().get('success'))
+        self.assertEqual(self.user.username, 'new_test_user')
+        self.assertTrue(self.user.check_password('new_test_password'))
+
+    def test_edit_of_username(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {'username': 'new_test_user'})
+        self.user.refresh_from_db()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTrue(response.json().get('success'))
+        self.assertEqual(self.user.username, 'new_test_user')
+
+    def test_edit_of_password(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {
+            'new_password1': 'new_test_password',
+            'new_password2': 'new_test_password',
+        })
+        self.user.refresh_from_db()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTrue(response.json().get('success'))
+        self.assertTrue(self.user.check_password('new_test_password'))
+
+    def test_duplicate_username(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {'username': 'test_user'})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertFalse(response.json().get('success'))
+        self.assertIn('Пользователь с таким именем уже существует', response.json()['errors']['username'])
+
+    def test_bad_password(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {
+            'new_password1': '1234',
+            'new_password2': '1234',
+        })
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn('Введённый пароль слишком короткий', response.json()['errors']['new_password2'])
+
+    def test_password_missmatch(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {
+            'new_password1': 'test_password_1',
+            'new_password2': 'test_password_2',
+        })
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn('Пароли не совпадают', response.json()['errors']['new_password2'])
+
+    def test_password_first_field_empty(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {'new_password2': 'test_password_2'})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn('Обязательное поле', response.json()['errors']['new_password1'])
+
+    def test_password_second_field_empty(self):
+        path = reverse('users:edit-credentials')
+        response = self.client.post(path, {'new_password1': 'test_password_1'})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn('Обязательное поле', response.json()['errors']['new_password2'])
