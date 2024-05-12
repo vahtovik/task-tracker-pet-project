861a253c5c631c5d1d7c27b033b0d14fc431477a
Artem Davtyan <artem_davtyan7@mail.ru>
2024-05-12 22:04:40 +0300

    add import dev and prod settings to main settings file

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -4,17 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*)6@)=qgaen^^x8nh59as@bangix*ef9xev%710g0($h2mjlhz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['0.0.0.0', '188.64.13.3', '127.0.0.1']
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -58,16 +47,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_tracker.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -102,9 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 LOGIN_URL = '/login/'
 
@@ -112,3 +88,8 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .dev_settings import *
+except ImportError:
+    from .prod_settings import *
